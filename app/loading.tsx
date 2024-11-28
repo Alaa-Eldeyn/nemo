cd3cd01cc23e764eb2003034730a766c895bb698
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-11-28 20:11:07 +0200

    feat: add loader animation styles to globals.css

---
+++ app/loading.tsx
@@ -0,0 +1,522 @@
+function loading() {
+  return (
+    /* From Uiverse.io by Pradeepsaranbishnoi */
+    <div className="loader">
+      <div className="cubes">
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="cube">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default loading;
